4bc4f28e650b2bc6269ec8c297298dac797bedca
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-18 16:06:46 +0900

    :gift: feat: 페이지네이션 구현
    그러나 페이지를 이동할 때 loading... 이 생긴다

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -3,22 +3,26 @@ import { useQuery } from "react-query"; // 서버에서 fetch 데이터를 할�
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
   // const data = [];
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
-    staleTime: 2000,
-  }); // 쿼리의 이름 (여기서는 posts) , staleTime 2초
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  ); // 쿼리의 이름 (여기서는 posts) , staleTime 2초
   if (isLoading) return <h3>Loading...</h3>;
   if (isError)
     return (
@@ -42,11 +46,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
