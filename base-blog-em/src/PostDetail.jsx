05d79d8b1cbb2374cfdb528c2f6e3a38640e7caf
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-18 15:35:10 +0900

    :gift: feat: 댓글 가져오기 기능 추가
    그런데 클릭해도 같은 댓글들을 가져오는 이슈가 있음

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,21 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  // const data = [];
+
+  const { data, isLoading, isError, error } = useQuery("comments", () =>
+    fetchComments(post.id)
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError)
+    return (
+      <>
+        <h3>Error</h3>
+        <p>${error.toString()}</p>
+      </>
+    );
 
   return (
     <>
