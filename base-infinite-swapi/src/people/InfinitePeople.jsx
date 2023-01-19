28bccae9d23a022001ee3f3f221b58f027750c20
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-19 22:16:22 +0900

    :gift: feat: useInfiniteQuery 페칭(Fetching)과 에러 상태
    로딩중 또한 구현

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -9,26 +9,40 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
     "sw-people",
     ({ pageParam = initialUrl }) => fetchUrl(pageParam),
     { getNextPageParam: (lastPage) => lastPage.next || undefined }
   );
 
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
   console.log(data);
 
   return (
-    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
-      {data.pages.map((pageData) =>
-        pageData.results.map((person) => (
-          <Person
-            key={person.name}
-            name={person.name}
-            hairColor={person.hair_color}
-            eyeColor={person.eye_color}
-          />
-        ))
-      )}
-    </InfiniteScroll>
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) =>
+          pageData.results.map((person) => (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
   );
 }
