b63535bfc7ea0603a4580cbef182f6859e111281
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-20 18:24:29 +0900

    :gift: feat: staff treatment별로 selector 처리
    useCallback을 이용하여 filter state인 체크박스가 바뀌면 seletor 함수가 호출되어 변경되게 구현

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
@@ -21,7 +21,15 @@ interface UseStaff {
 export function useStaff(): UseStaff {
   const [filter, setFilter] = useState('all');
   const fallback = [];
-  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
+
+  const selectFn = useCallback(
+    (unfilteredStaff) => filterByTreatment(unfilteredStaff, filter),
+    [filter],
+  );
+
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff, {
+    select: filter === 'all' ? undefined : selectFn,
+  });
 
   return { staff, filter, setFilter };
 }
