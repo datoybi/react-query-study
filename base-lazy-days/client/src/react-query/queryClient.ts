90ce9363be8771ca4efc8f73dd69a13e031a3015
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-20 19:30:40 +0900

    :gift: feat: 전역에서 refetch 제한 설정

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -19,6 +19,11 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       onError: queryErrorHandler,
+      staleTime: 600000, // 10 minutes,
+      cacheTime: 900000, // 15 minutes,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
     },
   },
 });
