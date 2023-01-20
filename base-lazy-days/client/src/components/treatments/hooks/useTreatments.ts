cd979bef87261b78271e4c9983fa568227bd3e70
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-20 18:54:10 +0900

    :gift: feat: refetch 제한 설정

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,11 +12,21 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {});
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 minutes
+    cacheTime: 900000, // 15 minutes (staleTime이 cacheTime을 staleTime을 초과하는게 말이 안된다!)
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
+
   return data;
 }
 
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000,
+    cacheTime: 900000,
+  });
 }
