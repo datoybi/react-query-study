76f6dc3b309991d2b53a08facc2e653bfc5468bb
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-20 15:24:48 +0900

    :wrench: chore: useAppointments.ts 주석 삭제

--- base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useAppointments.ts
@@ -46,52 +46,15 @@ interface UseAppointments {
 //		3a. 현재 년 월의 해당되는 일정들을 반환한다.
 
 export function useAppointments(): UseAppointments {
-  /** ****************** START 1: monthYear state *********************** */
-  // get the monthYear for the current date (for default monthYear state)
-  // 현재 날짜에서 년, 월을 얻는다 (default 년월 상태)
   const currentMonthYear = getMonthYearDetails(dayjs());
-
-  // state to track current monthYear chosen by user
-  // state value is returned in hook return object
-  // 유저가 선택한 현재 년 월을 추적한다.
-  // 상태닶은 훅에서 객체로 반환된다.
   const [monthYear, setMonthYear] = useState(currentMonthYear);
-  // setter to update monthYear obj in state when user changes month in view,
-  // returned in hook return object
-  // 유저가 월을 바꿨을 때, 년월 객체를 업데이트하기위해 세팅한다.
-  // 훅에서 객체를 반환한다.
+
   function updateMonthYear(monthIncrement: number): void {
     setMonthYear((prevData) => getNewMonthYear(prevData, monthIncrement));
   }
-  /** ****************** END 1: monthYear state ************************* */
-  /** ****************** START 2: filter appointments  ****************** */
-  // State and functions for filtering appointments to show all or only available
-  // 모든 일정과 가능한 일정을 보여주기 위한 일정을 필터링하는 상태와 함수이다. (체크박스)
+
   const [showAll, setShowAll] = useState(false);
-
-  // We will need imported function getAvailableAppointments here
-  // We need the user to pass to getAvailableAppointments so we can show
-  //   appointments that the logged-in user has reserved (in white)
-  // getAvailableAppointments 함수를 import 해야한다.
   const { user } = useUser();
-
-  /** ****************** END 2: filter appointments  ******************** */
-  /** ****************** START 3: useQuery  ***************************** */
-  // useQuery call for appointments for the current monthYear
-
-  // TODO: update with useQuery!
-  // Notes:
-  //    1. appointments is an AppointmentDateMap (object with days of month
-  //       as properties, and arrays of appointments for that day as values)
-  //
-  //    2. The getAppointments query function needs monthYear.year and
-  //       monthYear.month
-
-  // useQuery를 이용하여 현재 년월의 일정을 가져온다
-
-  // 할일: useQuery로 업데이트 하기
-  // 1. 일정은  AppointmentDateMap
-  // 2. getAppointments 쿼리 함수는 monthYear.year와 monthYear.month를 필요로 한다.
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -108,8 +71,6 @@ export function useAppointments(): UseAppointments {
     [queryKeys.appointments, monthYear.year, monthYear.month],
     () => getAppointments(monthYear.year, monthYear.month),
   );
-
-  /** ****************** END 3: useQuery  ******************************* */
 
   return { appointments, monthYear, updateMonthYear, showAll, setShowAll };
 }
