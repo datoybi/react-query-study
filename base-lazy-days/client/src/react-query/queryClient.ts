21813b5c51d338c47ed7878b985748a8c8d07468
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-01-21 14:11:45 +0900

    :gift: feat: mutation 전역 설정
    로딩과 에러 전역적인 설정

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -25,5 +25,8 @@ export const queryClient = new QueryClient({
       refetchOnReconnect: false,
       refetchOnWindowFocus: false,
     },
+    mutations: {
+      onError: queryErrorHandler,
+    },
   },
 });
